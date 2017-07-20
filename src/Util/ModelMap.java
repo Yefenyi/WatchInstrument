53a0a4789b9520f880552f9c0a1a2b3f442a22ad
Yefenyi <yefenyi@126.com>
2017-07-20 16:53:09 +0800

    draft

--- src/Util/ModelMap.java
+++ src/Util/ModelMap.java
@@ -11,6 +11,11 @@ public class ModelMap {
     	    	 return new EuclideanModel();
     	     case "UserModel":
     	    	 return new UserModel("src/UserModel/myMlPerceptron.nnet");
+    	     case "UserModelLeft":
+    	    	 return new UserModel("src/UserModel/myMlPerceptronL1.nnet");
+    	     case "UserModelRMatt":
+    	    	 return new UserModel("src/UserModel/myMlPerceptronRMatt.nnet");
+ 
     	 default:
     		 return null;
     	 }
